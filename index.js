5042b32420cd6eee2b070501de4ad43f4f04cb59
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 18:15:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
-function destructivelyAppendKitten(name) {
-  kittens.push(name)
+function destructively-@@@-Kitten(name) {
+  kittens.(name)
 }
